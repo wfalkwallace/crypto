eb95b4af2b5f54b7b7a3d0493accd82d04eb0be1
William Falk-Wallace <wfalkwallace@gmail.com>
2013-08-19 09:51:13 -0700

    adds vigenere pencrypt

--- vigenere.c
+++ vigenere.c
@@ -15,23 +15,26 @@
 #include <string.h>
 #include <ctype.h>
 
-
-
 //encrypt and print
-int pencrypt(const char *plain, int offset)
+int pencrypt(const char *plain, const char *k)
 {
 	//get string length and make non-const string copy
 	int length = strlen(plain);
+	int keylength = strlen(k);	
 	char cipher[length];
+	char key[keylength];
 	strncpy(cipher, plain, length + 1);
+	//doesnt account for spaces in key yet. that sticks it to ascii too
+	strncpy(key, k, keylength + 1);
 
 	//iterate over the original string
-	int i, j;
+	int i, j; //j->cipher; i->plain
 	for(i = j = 0; i < length; i++, j++) 
 	{
+
 		//encrypt character
 		if(!isspace(cipher[i]))
-			cipher[j] = ((toupper(cipher[i]) - 65) *  + offset) % 26 + 65;
+			cipher[j] = (toupper(cipher[i]) - 65 + toupper(key[(i % keylength)]) - 65) % 26 + 65;
 		//or step-back the ciphertext index if it's a space
 		else
 			j--;
@@ -115,21 +118,23 @@ FILE *fdecrypt(char *input, char *output, int offset)
 //Main/Test function
 int main(int argc, char *argv[])
 {
-	//proper usage required
-	if (argc != 5) {
-		fprintf(stderr, "Usage: %s <e/d> <input-file> <output-file> <offset>\n", argv[0]);
-		exit(1);
-	}
 
-	//encrypt
-	if (toupper(*argv[1]) == 'E')
-		fencrypt(argv[2], argv[3], atoi(argv[4]));
-	//decrypt
-	else if (toupper(*argv[1]) == 'D')
-		fdecrypt(argv[2], argv[3], atoi(argv[4]));
-	//not e or d
-	else
-		die("Invlaid Option");
-	//done
-	return 0;
+	pencrypt("Hello World", "go");
+	//proper usage required
+	// if (argc != 5) {
+	// 	fprintf(stderr, "Usage: %s <e/d> <input-file> <output-file> <offset>\n", argv[0]);
+	// 	exit(1);
+	// }
+
+	// //encrypt
+	// if (toupper(*argv[1]) == 'E')
+	// 	fencrypt(argv[2], argv[3], atoi(argv[4]));
+	// //decrypt
+	// else if (toupper(*argv[1]) == 'D')
+	// 	fdecrypt(argv[2], argv[3], atoi(argv[4]));
+	// //not e or d
+	// else
+	// 	die("Invlaid Option");
+	// //done
+	// return 0;
 }
